365a4ccc05600bb44df622140176c46c64faa059
zRyyH <zryyh.br@gmail.com>
2025-07-11 15:56:21 -0300

    beta 1.1

--- collector/init_mongo.py
+++ collector/init_mongo.py
@@ -8,16 +8,13 @@ def init_mongo_db(
     password="signals1234",
     target_db="candles",
 ):
-    # Monta a URI com ou sem autenticação
     if username and password:
         uri = f"mongodb://{username}:{password}@{host}:{port}"
     else:
         uri = f"mongodb://{host}:{port}/"
 
-    # Conecta
     client = MongoClient(uri)
 
-    # Cria o banco (Mongo cria ao inserir dados)
     db = client[target_db]
 
     print(f"Banco '{target_db}' inicializado com sucesso.")
