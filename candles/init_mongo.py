e5601e0fddbfc62d57d8a4b49e9b9a2dbc9cdf72
zRyyH <zryyh.br@gmail.com>
2025-07-14 16:35:43 -0300

    beta 1.3

--- candles/init_mongo.py
+++ candles/init_mongo.py
@@ -2,10 +2,10 @@ from pymongo import MongoClient
 
 
 def init_mongo_db(
-    host="207.180.193.45",
+    host="147.93.7.253",
     port=27017,
-    username="signals",
-    password="signals1234",
+    username="mongodb",
+    password="3ReSGpsD6TFDF5ww",
     target_db="candles",
 ):
     if username and password:
